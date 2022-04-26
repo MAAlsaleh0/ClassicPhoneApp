45e925da631576b727e2cc1a020a7d2e13d337ee
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-26 16:05:17 -0700

    Initial Commit

---
+++ ClassicPhoneApp/ClassicPhoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicPhoneAppApp.swift
+//  ClassicPhoneApp
+//
+//  Created by Mohammed Alsaleh on 25/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct ClassicPhoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
